ab13b8f3737996ee78771d4ef65e1cff50036a2a
Gurpreet Ari <gurpreet.ari@gmail.com>
2016-01-19 12:20:26 +0530

    Fourth commit - conflict test

--- testgroup/src/main/java/test/project/testgroup/App.java
+++ testgroup/src/main/java/test/project/testgroup/App.java
@@ -2,14 +2,13 @@ package test.project.testgroup;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("How are you?");
-        System.out.println("Sreeja asked me to change this file.");
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("How are you?");
+		System.out.println("Sreeja asked me to change this file.");
+		System.out.println("Conflict test!");
+	}
 }
